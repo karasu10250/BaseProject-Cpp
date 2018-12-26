3ad9f0f46f6ba7355fb963b975f3c0b299dcc49a
jeffery2999 <44108351+jeffery2999@users.noreply.github.com>
2018-12-26 10:26:10 +0800

    Update print.java

--- src/print.java
+++ src/print.java
@@ -18,25 +18,8 @@ public class print {
 		score[7] = 60;
 		score[8] = 60;
 		
-		System.out.println("==========測驗結果==========");
-		Print.printEach(score);
-		Print.totalSum(score);
-		System.out.print(Print);
-		
-		System.out.println("============建議============");
-		Print.compare();
-		
-		System.out.println("==========評分依據==========");
-		
 
-		System.out.println("[性別 ]\r\n" + "不具意義\r\n" + "[性向 ]\r\n" + "不具意義\r\n" + "[血型 ]\r\n" + "A(活潑 ) II.IV");
-		System.out.println("B(安靜 ) I.VIII\r\n" + "AB(興奮 ) IV.V.VI.IX\r\n" + "O(抑鬱 ) III.VII");
-		System.out.println("[星座 ]\r\n" + "I : 天蠍 .獅子\r\n" + "II : 水瓶 .天秤\r\n" + "III : 雙子\r\n" + "IV : 處女");
-		System.out.println("V : 魔羯\r\n" + "VI : 巨蟹.雙魚\r\n" + "VII : 牡羊\r\n" + "VIII : 金牛\r\n" + "XI : 射手");
-		System.out.println("------------------------");
-		System.out.println("VIII.IX.I 同區");
-		System.out.println("II.III.IV 同區");
-		System.out.println("V.VI.VII  同區");
+		Print.print(socre);
 		
 	}
 
